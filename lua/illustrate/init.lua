c1969be1cf9eef767a90abf37a71f6d0cd73fd06
Rafael Papallas <rpapallas@gmail.com>
2024-02-23 22:32:17 +0000

    Improved main functions, adding support for using params.

--- lua/illustrate/init.lua
+++ lua/illustrate/init.lua
@@ -1,6 +1,16 @@
 local M = {}
 local Config = require("illustrate.config")
 
+local function get_os()
+	local fh, _ = assert(io.popen("uname -o 2>/dev/null","r"))
+    local osname
+	if fh then
+		osname = fh:read()
+	end
+
+	return osname or "Windows"
+end
+
 function M.setup(options)
     require("illustrate.config").setup(options)
 end
@@ -11,14 +21,17 @@ end
 
 local function insert_include_code(filename)
     local filetype = vim.bo.filetype
-    local latex_svg_code = string.format("\\begin{figure}[htbp]\n\\centering\n\\includegraphics[width=\\linewidth]{%s}\n\\caption{caption}\n\\label{fig:label}\n\\end{figure}", filename)
-    local markdown_svg_code = string.format("![caption](%s)", filename)
+    local extension = filename:match("^.+%.(%w+)$")
 
     local insert_code = ""
-    if filetype == "tex" then
-        insert_code = latex_svg_code
-    elseif filetype == "markdown" then
-        insert_code = markdown_svg_code
+    if filetype == "tex" and extension == "svg" then
+        insert_code = Config.options.text_templates.svg.tex:gsub("$FILE_PATH", filename)
+    elseif filetype == "tex" and extension == "ai" then
+        insert_code = Config.options.text_templates.ai.tex:gsub("$FILE_PATH", filename)
+    elseif filetype == "md" and extension == "svg" then
+        insert_code = Config.options.text_templates.svg.md:gsub("$FILE_PATH", filename)
+    elseif filetype == "md" and extension == "ai" then
+        insert_code = Config.options.text_templates.ai.md:gsub("$FILE_PATH", filename)
     end
 
     if insert_code ~= "" then
@@ -40,9 +53,9 @@ local function insert_include_code(filename)
     end
 end
 
-local function create_figures_dir(figures_path)
-    -- TODO: make sure this command works on Windows too.
-    local ok, _ = os.execute("mkdir -p " .. figures_path)
+local function create_illustration_dir(illustration_dir_path)
+    local command = "mkdir -p " .. illustration_dir_path
+    local ok, _ = os.execute(command)
     if not ok then
         print("Failed to create the 'figures' directory.")
         return
@@ -50,39 +63,44 @@ local function create_figures_dir(figures_path)
 end
 
 function M.create_and_open_svg()
-    vim.notify = require("notify")
-    local figures_path = Config.options.illustration_dir
-    create_figures_dir(figures_path)
+    local illustration_dir_path = Config.options.illustration_dir
+    create_illustration_dir(illustration_dir_path)
 
     local filename = vim.fn.input("[SVG] Filename (w/o extension): ") .. ".svg"
-    local destination_filename = figures_path .. "/" .. filename
+    local destination_filename = illustration_dir_path .. "/" .. filename
 
-    -- TODO: get path from the config
-    -- TODO: allow user to pick templates from a template list?
-    local template_path = os.getenv("HOME") .. "/inbox/illustrate.nvim/templates/svg/template.svg"
+    local template_path = Config.options.template_files.directory.svg .. Config.options.template_files.default.svg
     copy_template(template_path, destination_filename)
     insert_include_code(destination_filename)
 
-    -- TODO: define app to open in the config; make this work for Windows and Linux.
-    os.execute("open -a 'Adobe Illustrator' " .. destination_filename)
+    local os_name = get_os()
+    local default_app = Config.options.default_app.svg
+    if default_app == 'inkscape' then
+        os.execute("inkscape " .. destination_filename)
+    elseif default_app == 'illustrator' and os_name == 'Darwin' then
+        os.execute("open -a 'Adobe Illustrator' " .. destination_filename)
+    end
 end
 
 function M.create_and_open_ai()
     vim.notify = require("notify")
-    local figures_path = Config.options.illustration_dir
-    create_figures_dir(figures_path)
+    local illustration_dir_path = Config.options.illustration_dir
+    create_illustration_dir(illustration_dir_path)
 
     local filename = vim.fn.input("[AI] Filename (w/o extension): ") .. ".ai"
-    local destination_filename = figures_path .. "/" .. filename
+    local destination_filename = illustration_dir_path .. "/" .. filename
 
-    -- TODO: get path from the config
-    -- TODO: allow user to pick templates from a template list?
-    local template_path = os.getenv("HOME") .. "/inbox/illustrate.nvim/templates/ai/template.ai"
+    local template_path = Config.options.template_files.directory.ai .. Config.options.template_files.default.ai
     copy_template(template_path, destination_filename)
     insert_include_code(destination_filename)
 
-    -- TODO: define app to open in the config; make this work for Windows and Linux.
-    os.execute("open -a 'Adobe Illustrator' " .. destination_filename)
+    local os_name = get_os()
+    local default_app = Config.options.default_app.svg
+    if default_app == 'inkscape' then
+        os.execute("inkscape " .. destination_filename)
+    elseif default_app == 'illustrator' and os_name == 'Darwin' then
+        os.execute("open -a 'Adobe Illustrator' " .. destination_filename)
+    end
 end
 
 return M
