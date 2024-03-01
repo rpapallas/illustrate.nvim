802b802e048af2d819cd1474a00b4886839b462c
Rafael Papallas <rpapallas@gmail.com>
2024-03-01 23:22:38 +0000

    Improved error handling.

--- lua/illustrate/utils.lua
+++ lua/illustrate/utils.lua
@@ -1,5 +1,6 @@
 local M = {}
 local Config = require("illustrate.config")
+vim.notify = require("notify")
 
 function M.get_os()
 	local fh, _ = assert(io.popen("uname -o 2>/dev/null","r"))
@@ -11,14 +12,24 @@ function M.get_os()
 	return osname or "Windows"
 end
 
+local function execute(command)
+    local handle = io.popen(command .. " 2>&1")
+    local result = handle:read("*a")
+    handle:close()
+
+    if result ~= "" then
+        vim.notify("Error: " .. result, "error")
+    end
+end
 
 function M.open(filename)
     local os_name = M.get_os()
     local default_app = Config.options.default_app.svg
+
     if default_app == 'inkscape' then
-        os.execute("inkscape " .. filename .. " >/dev/null 2>&1 &")
+        execute("inkscape " .. filename)
     elseif default_app == 'illustrator' and os_name == 'Darwin' then
-        os.execute("open -a 'Adobe Illustrator' " .. filename)
+        execute("open -a 'Adobe Illustrator' " .. filename)
     end
 end
 
