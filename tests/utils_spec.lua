23c2065a5c17f9d3bf1a7c58c68194b01003e1d5
Rafael Papallas <rpapallas@gmail.com>
2024-03-27 18:37:47 +0200

    Initialised unit testing.

---
+++ tests/utils_spec.lua
@@ -0,0 +1,7 @@
+local utils = require('illustrate.utils')
+
+-- describe('test operating system name', function()
+--   it('linux', function()
+--     assert.are.same(utils.get_os(), 'linux')
+--   end)
+-- end)
